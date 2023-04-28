f0197cd30ab123c947370d87fccc4fd2e9f3854b
MattNusbaum <m.guy008@gmail.com>
2023-04-28 15:11:07 -0700

    updated js

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  document.getElementById("foodsubmit").addEventListener("click", function () {
-    console.log(FoodArray);
+    document.getElementById("foodsubmit").addEventListener("click", function () {
+        console.log(FoodArray);
 
-    let xFoodname = document.getElementById("foodInput").value;
-    document.getElementById("calInput").value;
-    let xCalories = parseInt(calInput.value);
-    FoodArray.push(new Food(xFoodname, xCalories));
-    // clear text box
-    document.getElementById("foodInput").value = "";
-    document.getElementById("calInput").value = "";
-  });
+        let xFoodname = document.getElementById("foodInput").value;
+        document.getElementById("calInput").value;
+        let xCalories = parseInt(calInput.value);
+        FoodArray.push(new Food(xFoodname, xCalories));
+        // clear text box
 
-  // calorie calculation function call ?
-  // document.getElementById("foodsubmit").addEventListener("click", function () {
+        console.log(FoodArray);
 
-  // });
+        let totalCalories = 0;
+        for (let i = 0; i < FoodArray.length; i++) {
+            totalCalories += FoodArray[i].calories;
+        }
+        document.getElementById("totalOutput").value = totalCalories;
+        document.getElementById("foodInput").value = "";
+        document.getElementById("calInput").value = "";
+
+    });
+
+
+    // calorie calculation function call ?
+    // document.getElementById("foodsubmit").addEventListener("click", function () {
+
+    // });
 });
 
 // total calorie function:
